6e1ef420fd712871e384213dfa2996211f1fb604
NecrOctopuS <53893624+NecrOctopuS@users.noreply.github.com>
2020-10-18 23:13:14 +0300

    Add view for place

--- places/views.py
+++ places/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from places.models import Place
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+def show_main(request):
+    places = Place.objects.all()
+    features = []
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.latitude, place.longitude]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": {
+                    "title": place.title,
+                    "imgs": place.get_images_paths(),
+                    "description_short": place.description_short,
+                    "description_long": place.description_long,
+                    "coordinates": {
+                        "lng": place.longitude,
+                        "lat": place.latitude
+                    }
+                }
+            }
+        }
+        features.append(feature)
+    places_for_render = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    return render(request, "index.html", context={"places": places_for_render})
+
+
+def show_place(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    return HttpResponse(f'{place.title}')
